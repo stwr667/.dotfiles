53621c7e7c1326b6bd525612331e3fea9e8eb9d7
Stephen Wrathall <stephen.wrathall@agworld.com>
2019-12-05 16:07:09 +0800

    Install RVM, make vim not break every time.

--- vim/auto_setup.sh
+++ vim/auto_setup.sh
@@ -2,3 +2,6 @@
 # if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ]; then
 #   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 # fi
+
+# Makes this directory if it doesn't exist:
+mkdir -p ~/vimBackupFiles
