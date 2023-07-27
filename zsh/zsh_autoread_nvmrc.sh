5630b270d348ee0812df15c9ae7c1694cee0a02a
Stephen Wrathall <swrathall@atlassian.com>
2023-07-27 11:11:44 +0800

    Make sure that .nvmrc files in projects are respected

---
+++ zsh/zsh_autoread_nvmrc.sh
@@ -0,0 +1,31 @@
+# This exists so that when you `cd` into a dir, it will check the `.nvmrc`
+# file and load the right version of node.
+#
+# Source: https://github.com/nvm-sh/nvm#zsh
+#
+# Note that this does slow down the `cd` command. There's no better know solution atm.
+
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+
+load-nvmrc() {
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
